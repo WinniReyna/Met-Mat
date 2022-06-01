0aa06ef38b18f29539e3b9f4b124881136c5c7b8
Von Valentine <73485243+WinniReyna@users.noreply.github.com>
2022-05-31 20:31:50 -0500

    random number  and random mold

---
+++ Assets/Scripts/Juego/9Moldeo/RandomObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObject : MonoBehaviour
+{
+    [SerializeField] private GameObject[] mold;
+    int randomNumber;
+
+    private void Start()
+    {
+        randomNumber = Random.Range(0, 3);
+    }
+
+    void Update()
+    {
+        switch (randomNumber)
+        {
+            case 0:                
+                mold[0].SetActive(true);
+                mold[1].SetActive(false);
+                mold[2].SetActive(false);
+                StartCoroutine("RandomNumber");
+                break;
+            case 1:
+                mold[0].SetActive(false);
+                mold[1].SetActive(true);
+                mold[2].SetActive(false);
+                StartCoroutine("RandomNumber");
+                break;
+            case 2:
+                mold[0].SetActive(false);
+                mold[1].SetActive(false);
+                mold[2].SetActive(true);
+                StartCoroutine("RandomNumber");
+                break;
+        }
+    }
+
+    IEnumerator RandomNumber()
+    {        
+        yield return new WaitForSeconds(5.8f);
+        randomNumber = Random.Range(0, 3);
+        StopAllCoroutines();
+    }
+}
